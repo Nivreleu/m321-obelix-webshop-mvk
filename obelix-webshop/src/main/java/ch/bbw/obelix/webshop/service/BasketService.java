8531d0eacc35491864af0c426d49e5391a0e86ea
Nivre0 <timleu256@gmail.com>
2025-11-17 10:47:11 +0100

    moved basket from webshop stuff

---
+++ obelix-webshop/src/main/java/ch/bbw/obelix/webshop/service/BasketService.java
@@ -0,0 +1,69 @@
+﻿package ch.bbw.obelix.webshop.service;
+
+import ch.bbw.obelix.webshop.controller.ObelixWebshopController;
+import ch.bbw.obelix.webshop.dto.BasketDto;
+import ch.bbw.obelix.webshop.dto.DecorativenessDto;
+import ch.bbw.obelix.webshop.entity.MenhirEntity;
+import ch.bbw.obelix.webshop.repository.MenhirRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.StandardException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.*;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BasketService {
+    @Lazy
+    private final ObelixWebshopController quarryWebclient;
+
+    private BasketDto basket;
+
+    static <T> List<T> append(List<T> immutableList, T element) {
+        var tmpList = new ArrayList<>(immutableList);
+        tmpList.add(element);
+        return Collections.unmodifiableList(tmpList);
+    }
+
+    public BasketDto offer(@NonNull BasketDto.BasketItem basketItem) {
+        basket = basket.withItems(append(basket.items(), basketItem));
+        return basket;
+    }
+
+    @PostConstruct
+    public void leave() {
+        basket = BasketDto.empty();
+    }
+
+    public boolean isGoodOffer(DecorativenessDto decorativeness) {
+        var stoneWorth = decorativeness.ordinal();
+        var basketWorth = basket.items()
+                .stream().map(x -> switch (x.name().toLowerCase(Locale.ROOT)) {
+                    case "boar" -> 5; // oh boy, oh boy!
+                    case "honey" -> 2;
+                    case "magic potion" -> 0; // not allowed to drink this!
+                    default -> 1; // everything is worth something
+                } * x.count()).reduce(0, Integer::sum);
+        log.info("basket worth {} vs menhir worth {} ({})", basketWorth, decorativeness, stoneWorth);
+        return basketWorth >= stoneWorth;
+    }
+
+    public void exchange(UUID menhirId) {
+        var menhir = quarryWebclient.getMenhirById(menhirId);
+        var decorativeness = menhir.decorativeness();
+        if (!isGoodOffer(decorativeness)) {
+            throw new ObelixWebshopService.BadOfferException("Bad Offer: That won't even feed Idefix!");
+        }
+        quarryWebclient.deleteById(menhirId);
+        leave();
+    }
+}
--- obelix-webshop/src/main/java/ch/bbw/obelix/webshop/service/ObelixWebshopService.java
+++ obelix-webshop/src/main/java/ch/bbw/obelix/webshop/service/ObelixWebshopService.java
@@ -31,51 +31,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequiredArgsConstructor
 public class ObelixWebshopService {
 
-	@Lazy
-	private final ObelixWebshopController quarryWebclient;
-
 	private final MenhirRepository menhirRepository;
-
-	private BasketDto basket;
-
-	static <T> List<T> append(List<T> immutableList, T element) {
-		var tmpList = new ArrayList<>(immutableList);
-		tmpList.add(element);
-		return Collections.unmodifiableList(tmpList);
-	}
-
-	public BasketDto offer(@NonNull BasketDto.BasketItem basketItem) {
-		basket = basket.withItems(append(basket.items(), basketItem));
-		return basket;
-	}
-
-	@PostConstruct
-	public void leave() {
-		basket = BasketDto.empty();
-	}
-
-	public boolean isGoodOffer(DecorativenessDto decorativeness) {
-		var stoneWorth = decorativeness.ordinal();
-		var basketWorth = basket.items()
-				.stream().map(x -> switch (x.name().toLowerCase(Locale.ROOT)) {
-					case "boar" -> 5; // oh boy, oh boy!
-					case "honey" -> 2;
-					case "magic potion" -> 0; // not allowed to drink this!
-					default -> 1; // everything is worth something
-				} * x.count()).reduce(0, Integer::sum);
-		log.info("basket worth {} vs menhir worth {} ({})", basketWorth, decorativeness, stoneWorth);
-		return basketWorth >= stoneWorth;
-	}
-
-	public void exchange(UUID menhirId) {
-		var menhir = quarryWebclient.getMenhirById(menhirId);
-		var decorativeness = menhir.decorativeness();
-		if (!isGoodOffer(decorativeness)) {
-			throw new BadOfferException("Bad Offer: That won't even feed Idefix!");
-		}
-		quarryWebclient.deleteById(menhirId);
-		leave();
-	}
 
 	@PostConstruct
 	public void initializeMenhirs() {
